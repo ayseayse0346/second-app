48c6431fbd8d38487540a5d52591c1dd88667c04
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-04-17 20:29:13 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
-import './App.css';
-
+// ?Component
+// reactte componentler fonksiyon gibi yazılır ama html döndürürler.fonksiyondan farkı baş harfi büyük olur.ve bu fonksiyonlar jsx döndürürler.
 function App() {
   return (
+    // ! Jsx(HTML-JS'nin birleşmiş halidir.)
     <div className="App">
      <h2>Hoşgeldin Ya şehri react</h2>
     </div>
